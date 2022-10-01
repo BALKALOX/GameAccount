c52c39e07a5a078998a24db4070fe5b11d89837c
BALKALOX <misiura.anton@lll.kpi.ua>
2022-10-01 12:40:10 +0300

    Add project files.

---
+++ GameAccount.cs
@@ -0,0 +1,40 @@
+﻿namespace GameAccount
+{
+    public class GameAccount
+    {
+        public string UserName;
+        public string OponentName;
+        public int CurrentRating;
+        public int GameCount;
+        public int num;
+        public List<string> list;
+
+        public GameAccount(string UN, string ON)
+        {
+            this.OponentName = ON;
+            this.UserName = UN;
+            this.CurrentRating = 0;
+            this.GameCount = 0;
+            this.list = new List<string>();
+        }
+        public void GameWin(int CurrentRating, int GameCount, string OponentName)
+        {
+            Console.WriteLine("!_!_!VICTORY!_!_!");
+            this.CurrentRating += 10;
+            Console.WriteLine(OponentName+" lost\n Your rate " + this.CurrentRating);
+            this.GameCount++;
+        }
+        public void GameLoose(int CurrentRating, int GameCount, string OponentName)
+        {
+            Console.WriteLine("._._.WASTED!_._.");
+            if (CurrentRating <= 0) { CurrentRating = 0; }
+            else { this.CurrentRating -= 10; }
+            Console.WriteLine(OponentName + " won\n Your rate " + this.CurrentRating);
+            this.GameCount++;
+        }
+        public string toString(string UN, string ON, string CR, string GC)
+        {
+            return ("UserName:"+UN+"\nOponentName :"+ON+ "\nRate :" + CR + "\nGame :" + GC);
+        }
+    }
+}
